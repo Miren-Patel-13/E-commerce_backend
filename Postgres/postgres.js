be0bc8990cbc3d7b6c6db27e7b4c1dc1f15a70da
patelmiren13@gmail.com <patelmiren13@gmail.com>
2025-05-26 22:58:35 +0530

    Add file for DB connection and add db connection to server.js

---
+++ Postgres/postgres.js
@@ -0,0 +1,17 @@
+import { Sequelize } from "sequelize";
+
+const sequelize = new Sequelize('postgres', 'sa', 'sa123', {
+    host: 'localhost',
+    dialect: 'postgres'
+});
+
+const connection = async () => {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+}
+
+export { connection };
